f31139902067cbd8728aa6f1a433bfedb2e57fc7
William Cofer <willcofer@gmail.com>
2021-01-21 05:25:03 -0600

    styling cleanup

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="post">
     <h1> {{ post.name.first }} {{ post.name.last }} </h1>
-    <hr>
+    <hr style="border:1px solid #c20017;">
     <h4> {{ post.email}} </h4>
     <h4> {{ post.phone}} </h4>
   <!--  <h4> {{ post.latitude }}, {{ post.longitude }} </h4> -->
@@ -34,6 +34,8 @@ export default {
 
 
 <style scoped>
+.post {
+}
 .ml {
 margin-left:10px;
 }
--- src/components/PostContainer.vue
+++ src/components/PostContainer.vue
@@ -59,6 +59,9 @@ padding: 10px 0 15px 0;
 border-radius: 30px;
 background-color: #003366;
 color: white;
+-webkit-box-shadow: 3px 3px 5px 6px #ccc;  /* Safari 3-4, iOS 4.0.2 - 4.2, Android 2.3+ */
+-moz-box-shadow:    3px 3px 5px 6px #ccc;  /* Firefox 3.5 - 3.6 */
+box-shadow:         3px 3px 5px 6px #ccc;
 }
 .title {
 color:#c20017;font-size:45px;
